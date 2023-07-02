036751f1d2a4137e0f9a192e29c2932515dfcee2
Ty <ty.jvile@gmail.com>
2023-07-02 21:24:01 +0930

    add grafbase

--- components/Auth/AuthProviders.tsx
+++ components/Auth/AuthProviders.tsx
@@ -1,4 +1,19 @@
+import { getProviders, signIn } from "next-auth/react";
+
+import { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  singinUrlParams: string;
+};
+
 function AuthProviders() {
+  const [providers, setProviders] = useState<Providers | null>(null);
+
   return <div>AuthProviders</div>;
 }
 
