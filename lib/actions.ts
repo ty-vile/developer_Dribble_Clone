cd04db88febd13efe64c02e294d2da72a414437c
Ty <typhn.sa@gmail.com>
2023-07-22 12:22:19 +0930

    add log to fetchToken/actions.ts testing

--- lib/actions.ts
+++ lib/actions.ts
@@ -12,13 +12,12 @@ import { ProjectForm } from "@/types";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === "production";
-
 const apiUrl = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
   : "http://127.0.0.1:4000/graphql";
 const apiKey = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
-  : "noapifound";
+  : "letmein";
 const serverUrl = isProduction
   ? process.env.NEXT_PUBLIC_SERVER_URL
   : "http://localhost:3000";
@@ -53,6 +52,7 @@ export const createUser = (name: string, email: string, avatarUrl: string) => {
 
 export const fetchToken = async () => {
   try {
+    console.log(serverUrl);
     const response = await fetch(`${serverUrl}/api/auth/token`);
 
     return response.json();
